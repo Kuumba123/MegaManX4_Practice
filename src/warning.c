9aea8d3cf5c0f754d959a4f6652180c7f710e8a9
PogChampGuy <Kuumba.Lambie@gmail.com>
2025-03-12 23:48:21 -0400

    fixed Owl's Warning

--- src/warning.c
+++ src/warning.c
@@ -1,5 +1,6 @@
 #include <common.h>
 #include <object.h>
+#include <layer.h>
 
 #define arcEffectP (*(Object **)((int)objP + 0x14))
 
@@ -7,6 +8,10 @@ void WarningObject(Object *objP)
 {
     if (arcEffectP->flags == 0)
     {
+        if (game.stageId == 0x7 && bgLayers[0].x != 0x8C0)
+        {
+            return;
+        }
         objP->act = 2;
     }
 }
